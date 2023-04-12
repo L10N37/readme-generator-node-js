1a7743f70c1372b8b552f6ce9b5a952d47d1d830
Lionel Sanderson <lfsanderson@hotmail.comgit config --global user.name Lionel>
2023-04-12 19:59:00 +1000

    add license badge stuff, not finalised

--- Develop/index.js
+++ Develop/index.js
@@ -33,9 +33,44 @@ THEN I am taken to the corresponding section of the README
 */
 
 // The main code here was initially created by ChatGPT! This was modified to suit the project requirements.
+const licensebadges = {
+    apache: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    boostSWL1: '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)',
+    BSD3clause: '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
+    BSD2clause: '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)',
+    CC0: '[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)',
+    attribution4: '[![License: CC BY 4.0](https://licensebuttons.net/l/by/4.0/80x15.png)](https://creativecommons.org/licenses/by/4.0/)',
+    attributionShareAlike4Int: '[![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC_BY--SA_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-sa/4.0/)',
+    attributionNonCommercial4Int: '[![License: CC BY-NC 4.0](https://img.shields.io/badge/License-CC_BY--NC_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc/4.0/)',
+    attributionNoDerivatives4Int: '[![License: CC BY-ND 4.0](https://img.shields.io/badge/License-CC_BY--ND_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nd/4.0/)',
+    attributionNonCommercialShareAlike4: '[![License: CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC_BY--NC--SA_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)',
+    attributionNonCommercialNoDerivatives4Int: '[![License: CC BY-NC-ND 4.0](https://img.shields.io/badge/License-CC_BY--NC--ND_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-nd/4.0/)',
+    eclipsePublicLicense1: '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)',
+    GNUGPLV3: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+    GNUGPLV2:  '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)',
+    GNUAGPLV3: '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)',
+    GNULGPLV3:  '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)',
+    GNUFDLV1_3: '[![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)',
+    hippocratic2_1: '[![License: Hippocratic 2.1](https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg)](https://firstdonoharm.dev)',
+    hippocratic3: '[![License: Hippocratic 3.0](https://img.shields.io/badge/License-Hippocratic_3.0-lightgrey.svg)](https://firstdonoharm.dev)',
+    IBMPublic1: '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)',
+    ISC: '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
+    MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    mozillaPublic2: '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)',
+    attributionBY: '[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](https://opendatacommons.org/licenses/by/)',
+    openDatabaseLicense: '[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)',
+    PDDL: '[![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)',
+    perl: '[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)',
+    artistic2: '[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)',
+    SILOpenFontLicense: '[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)',
+    theUnlicense: '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)',
+    WTFPL: '[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)',
+    zlib: '[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)'
+};
 
 const fs = require("fs");
 const readline = require("readline");
+let license= '';
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -44,6 +79,7 @@ const rl = readline.createInterface({
 
 const generateReadme = (data) => {
   return `
+
 # ${data.title}
 <br>
 ${data.description}
@@ -64,11 +100,6 @@ ${data.installation}
 ${data.usage}
 <br>
 
-## License
-<br>
-${data.license}
-<br>
-
 ## Contributing
 <br>
 ${data.contributing}
@@ -87,17 +118,22 @@ ${data.questions}
 `;
 };
 
+
+function renderLicenseBadge(license) {
+license = "testing";
+    return license;
+}
+
 rl.question("Enter title: ", (title) => {
     rl.question("Enter table of contents: ", (TOC) => {
         rl.question("Enter description: ", (description) => {
             rl.question("Enter installation instructions: ", (installation) => {
                 rl.question("Enter usage information: ", (usage) => {
-                    rl.question("Enter license: ", (license) => {
-                        rl.question("Enter contribution guidelines: ", (contributing) => {
-                            rl.question("Enter test instructions: ", (tests) => {
+                    rl.question("Enter contribution guidelines: ", (contributing) => {
+                        rl.question("Enter test instructions: ", (tests) => {
                                 rl.question("Enter questions: ", (questions) => {
-          const data = { title, TOC, description, installation, usage, license, contributing, tests, questions };
-          const readme = generateReadme(data);
+          const data = { title, TOC, description, installation, usage, contributing, tests, questions };
+          const readme = renderLicenseBadge(license)+generateReadme(data);
           fs.writeFile("README.md", readme, (err) => {
             if (err) throw err;
             console.log("README.md generated!");
@@ -111,4 +147,3 @@ rl.question("Enter title: ", (title) => {
             });
         });
     });
-});
